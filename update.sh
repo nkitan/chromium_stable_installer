be6aaa6dd16ffb2da547476f2d74c5b92fe9f46a
Ankit Das <ankitdas2k@gmail.com>
2020-12-29 02:00:18 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-cd $(dirname $0)
+cd $(dirname $0) 
 
-LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
+LASTCHANGE_URL="https://github.com/nkitan/chromium-latest-linux/blob/master/current_latest_revision.txt"
 
 REVISION=$(curl -s -S $LASTCHANGE_URL)
 
@@ -14,14 +14,22 @@ if [ -d $REVISION ] ; then
 fi
 
 ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
-
 ZIP_FILE="${REVISION}-chrome-linux.zip"
 
 echo "fetching $ZIP_URL"
-
+ 
+echo "deleting previous installation"
 rm -rf $REVISION
 mkdir $REVISION
 pushd $REVISION
+
+if [ $? == 0 ] ; then
+        echo "removed successfully, proceeding to install"
+else
+        echo "couldn't remove previous installation, exiting"
+        exit
+fi
+
 curl -# $ZIP_URL > $ZIP_FILE
 echo "unzipping.."
 unzip $ZIP_FILE
